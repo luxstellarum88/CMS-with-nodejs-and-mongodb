836a5be2b90ecf20b88b3755e0b799bec39734bb
root <root@ubuntu.ubuntu-domain>
2012-07-04 21:59:42 +0900

    when make account, check ID to avoid overlapping. (120704 JH)

--- account/makeaccount.js
+++ account/makeaccount.js
@@ -8,28 +8,37 @@ exports.insertUser = function(req, res){
 	var useridentity = dbModel.makeUserModel();
 	var user = req.body;
 	
-	useridentity.Id = user.idForm;
-	useridentity.password = user.pwForm;
-	useridentity.name = user.nameForm;
-	useridentity.email = user.emailForm;
-	useridentity.role = 'Guest';
-	
-	useridentity.save(function(err){
-		if(!err)
-			console.log('User_inser_success');
-		else
-			res.redirect('/');
-			
-		usersModel.authenticate(user.idForm, user.pwForm, function(user){			
-			if(user){
-				req.session.user_id = user.Id;
-				res.redirect('/board');
-			}
-			else{
-				res.render('sessions/new', {title: 'login', locals : 
-					{redir: '/board'}	
-				});
-			}
-		});		
-	});
+	//add at 120704. JH
+	var userModel = dbModel.tossUserModel();
+	if( !userModel.findOne({Id:user.idForm}) ) {
+		
+		useridentity.Id = user.idForm;
+		useridentity.password = user.pwForm;
+		useridentity.name = user.nameForm;
+		useridentity.email = user.emailForm;
+		useridentity.role = 'Guest';
+		
+		useridentity.save(function(err){
+			if(!err)
+				console.log('User_inser_success');
+			else
+				res.redirect('/');
+				
+			usersModel.authenticate(user.idForm, user.pwForm, function(user){			
+				if(user){
+					req.session.user_id = user.Id;
+					res.redirect('/board');
+				}
+				else{
+					res.render('sessions/new', {title: 'login', locals : 
+						{redir: '/board'}	
+					});
+				}
+			});		
+		});
+	}
+	else {
+		console.log('existed ID');
+		res.redirect('/');
+	}
 }
