564fffb0233b6817d90a2a23146014eaec589f2b
Son Junhong <luxstellarum@skima.co.kr>
2012-07-07 02:12:22 +0900

    add search function in board

--- board/board_search.js
+++ board/board_search.js
@@ -4,21 +4,55 @@ dbModel.connectBoardDB();
 
 exports.board_search = function(search, callback) {
 	var board_model = dbModel.tossBoardModel();
-	var search_reg_exp = '/'+search.content+'/i';	
-	var type = search.type;
+	var search_reg_exp = new RegExp(search.content,'i');	
 	var result = new Array;
 	
-		
-	board_model.find( { type : search_reg_exp }, function(err, docs) {
-		if(!err) {
-			console.log("type : " + type);
-			console.log("reg_exp : " + search_reg_exp);
-			result = docs;
-			console.log("result : " + result );
-			callback(result);
-		}//end of if
-		else {
-			console.log("search error");
-		}
-	});//end of find function
+	if('Id' === search.type)
+	{
+		board_model.find( { Id : search_reg_exp }, function(err, docs) {
+			if(!err) {
+				result = docs;
+				callback(result);
+			}//end of if
+			else {
+				console.log("search error");
+			}
+		});//end of find function
+	}
+	else if('name' === search.type) {
+		board_model.find( { name : search_reg_exp }, function(err, docs) {
+			if(!err) {
+				result = docs;
+				callback(result);
+			}//end of if
+			else {
+				console.log("search error");
+			}
+		});//end of find function
+	}
+	else if('subject' === search.type) {
+		board_model.find( { subject : search_reg_exp }, function(err, docs) {
+			if(!err) {
+				result = docs;
+				callback(result);
+			}//end of if
+			else {
+				console.log("search error");
+			}
+		});//end of find function
+	}
+	else if('memo' === search.type) {
+		board_model.find( { memo : search_reg_exp }, function(err, docs) {
+			if(!err) {
+				result = docs;
+				callback(result);
+			}//end of if
+			else {
+				console.log("search error");
+			}
+		});//end of find function
+	}
+	else{
+		console.log("search error");
+	}
 }//end of board_search
