4ad7c8835af6f3164643a13c8730c035b4915fd3
RolandBlain <rolandblain@ubuntu.(none)>
2012-08-03 16:57:01 +0900

    404 custom error page

--- app.js
+++ app.js
@@ -30,11 +30,12 @@ app.configure(function(){
   }));
   app.use(express.methodOverride());
   app.use(app.router);
-  // app.use(function(req, res, next){
-  	// res.status(404);
-  	// res.render('404.jade', {title: "404 - 페이지를 찾을 수 없습니다.", url: req.url });
-  // });
   app.use(express.static(__dirname + '/public'));
+  
+  app.use(function(req, res, next){
+  	res.status(404);
+  	res.render('404', {title:"404 - 페이지를 찾을 수 없습니다.", session: req.session.user, url: req.url});
+  })
 });
 
 app.configure('development', function(){
